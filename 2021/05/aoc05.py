069188453680d6db5257fc92de053716e75f0451
David A. Riggs <david.a.riggs@gmail.com>
2021-12-05 20:53:16 -0500

    Day 5 Part 2

--- 2021/05/aoc05.py
+++ 2021/05/aoc05.py
@@ -14,20 +14,18 @@ X, Y = 0, 1  # the two coordinates of a point
 
 
 def main1(vectors):
+    main2(horizontals(vectors))
+
+def main2(vectors):
     w = max([max(v[A][X], v[B][X]) for v in vectors]) + 1
     h = max([max(v[A][Y], v[B][Y]) for v in vectors]) + 1
     grid = [[0 for _ in range(0, h)] for _ in range(0, w)]
     print(f'Initialized {w}x{h} grid.')
 
     print(f'Loaded {len(vectors)} vectors.')
-    vectors = horizontals(vectors)
-    print(f'Filtered to {len(vectors)} horizontals.')
-
     for v in vectors:
-        #print(f'{v[A]} -> {v[B]}')
         for p in iterate(v):
             grid[p[X]][p[Y]] += 1
-    #print_grid(grid)
     
     count = 0
     for row in grid:
@@ -40,17 +38,27 @@ def iterate(vector):
     """Generate all discrete points of a single vector"""
     dx = 1 if vector[A][X] <= vector[B][X] else -1
     dy = 1 if vector[A][Y] <= vector[B][Y] else -1
-    for x in range(vector[A][X], vector[B][X]+dx, dx):
-        for y in range(vector[A][Y], vector[B][Y]+dy, dy):
+
+    if vector[A][X] == vector[B][X] or vector[A][Y] == vector[B][Y]:
+        # horizontals / verticals
+        for x in range(vector[A][X], vector[B][X]+dx, dx):
+            for y in range(vector[A][Y], vector[B][Y]+dy, dy):
+                yield x, y
+    else:
+        # diagonals
+        x, y = vector[A][X], vector[A][Y]
+        while x != vector[B][X] and y != vector[B][Y]:
             yield x, y
+            x += dx
+            y += dy
+        yield vector[B]
 
 def horizontals(vectors):
     """Filter a list of vectors to only include "horizontal" vectors"""
     return [v for v in vectors if v[A][X] == v[B][X] or v[A][Y] == v[B][Y]]
 
 def print_grid(grid):
-    w = len(grid)
-    h = len(grid[0])
+    w, h = len(grid), len(grid[0])
     print('  ' + ''.join(str(i % 10) for i in range(0, w)))
     print('  ' + '-' * w)
     for i, row in enumerate(grid):
@@ -58,6 +66,7 @@ def print_grid(grid):
 
 
 def parse(lines):
+    """Parse input lines, producing vectors. A vector is a pair of X,Y coordinates, A (start) and B (end)."""
     for line in lines:
         toks = line.split()
         assert len(toks) == 3
@@ -72,3 +81,5 @@ if __name__ == '__main__':
     vectors = list(parse([line.strip() for line in infile]))
 
     main1(vectors)
+    print()
+    main2(vectors)
