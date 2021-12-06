569b2dbd112d393a4a5fbc3c95d585bda053264b
David A. Riggs <david.a.riggs@gmail.com>
2021-12-06 00:25:08 -0500

    Day 6 Part 1 bruteforce solution, obviously won't scale much higher

---
+++ 2021/06/aoc06.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+
+infile = sys.stdin if len(sys.argv) == 1 else open(sys.argv[1], 'r')
+
+school = map(int, infile.readline().split(','))
+
+def reproduce(fish):
+    if fish == 0:
+        yield 6
+        yield 8
+    else:
+        yield fish - 1
+
+
+for day in range(256):
+    school2 = []
+    for fish in school:
+        if fish == 0:
+            school2.append(6)
+            school2.append(8)
+        else:
+            school2.append(fish - 1)
+    school = school2
+
+print(f'{day:2d}\t{len(school)}')
