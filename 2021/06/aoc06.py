4f409b96e63ede424e71ab83a5deb113a1778ff9
David A. Riggs <david.a.riggs@gmail.com>
2021-12-06 19:37:45 -0500

    Day 6

--- 2021/06/aoc06.py
+++ 2021/06/aoc06.py
@@ -1,19 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+"""
+Advent Of Code 2021: Day 5
+https://adventofcode.com/2021/day/5
+
+Reads an input file of "fish" modeled as numbers, calculates exponential growth.
+"""
 
 import sys
+from collections import defaultdict
 
-infile = sys.stdin if len(sys.argv) == 1 else open(sys.argv[1], 'r')
 
-school = map(int, infile.readline().split(','))
+def main(fishes, days):
+    school = defaultdict(int)  # age -> count
 
-for day in range(1, 80+1):
-    school2 = []
-    for fish in school:
-        if fish == 0:
-            school2.append(6)
-            school2.append(8)
-        else:
-            school2.append(fish - 1)
-    school = school2
+    for fish in fishes:
+        school[fish] += 1  # initial state
 
-print(f'{day:2d}\t{len(school)}')
+    for day in range(1, days+1):
+        # iterate up from 1 to 8 since each tick propogates downward
+        # handle 0 last so they don't get counted twice
+        zeros = school[0]
+        for i in range(1, 8+1):
+            school[i-1] = school[i]
+        school[8] = zeros
+        school[6] += zeros
+    
+    print(f'Day {day}:\t{sum(school.values())} lanternfish')
+
+
+if __name__ == '__main__':
+    infile = sys.stdin if len(sys.argv) == 1 else open(sys.argv[1], 'r')
+    fishes = list(map(int, infile.readline().split(',')))
+    
+    main(fishes, 80)
+    main(fishes, 256)
