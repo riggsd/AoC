9dc3240c58891dc895d0c5a3424c1cff474c49a2
David A. Riggs <david.a.riggs@gmail.com>
2022-12-01 08:21:38 -0500

    cleanup

--- 2022/01/aoc01.py
+++ 2022/01/aoc01.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 import sys
+from typing import Iterator, TextIO
 
-counts = []
-total = 0
 
-for line in sys.stdin:
-    line = line.strip()
-    if not line:
-        counts.append(total)
-        total = 0
-        continue
-    total += int(line)
+def parse(infile: TextIO) -> Iterator[int]:
+    """Yield the total calorie count for each elf"""
+    total = 0
+    for line in infile:
+        line = line.strip()
+        if not line:
+            yield total
+            total = 0
+        else:
+            total += int(line)
+
+
+counts = list(parse(sys.stdin))
 
 top1 = max(counts)
 print(top1)
 
 top3 = sorted(counts)[-3:]
 print(sum(top3))
+
