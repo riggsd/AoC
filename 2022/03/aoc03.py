4a9039e1561e29047b2e125f110e2961660c4e28
David A. Riggs <david.a.riggs@gmail.com>
2022-12-03 01:35:17 -0500

    day 3

--- 2022/03/aoc03.py
+++ 2022/03/aoc03.py
@@ -9,17 +9,17 @@ def parse(infile) -> Iterator[str]:
         yield line.strip()
 
 
-def split2(infile) -> Iterator[Tuple[str, str]]:
+def split2(infile) -> Iterator[Tuple[set, set]]:
     for line in infile:
         n = len(line)
-        yield line[:n//2], line[n//2:]
+        yield set(line[:n//2]), set(line[n//2:])
 
 
-def split3(infile) -> Iterator[Tuple[str, str, str]]:
+def split3(infile) -> Iterator[Tuple[set, set, set]]:
     infile = iter(infile)
     for line1 in infile:
         line2, line3 = next(infile), next(infile)
-        yield line1, line2, line3
+        yield set(line1), set(line2), set(line3)
 
 
 def pri(item: str) -> int:
@@ -30,10 +30,10 @@ def priority(items: set[str]) -> int:
     return sum(pri(item) for item in items)
 
 
-input = list(parse(sys.stdin))
+input = list( parse(sys.stdin) )
 
-total1 = sum(priority(set(l) & set(r)) for (l, r) in split2(input))
+total1 = sum( priority(l & r) for (l, r) in split2(input) )
 print(total1)
 
-total2 = sum(priority(set(a) & set(b) & set(c)) for (a, b, c) in split3(input))
+total2 = sum( priority(a & b & c) for (a, b, c) in split3(input) )
 print(total2)
