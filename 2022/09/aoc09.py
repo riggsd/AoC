3a95d22bd1e149ca407528744e874846254c092e
David A. Riggs <david.a.riggs@gmail.com>
2022-12-09 23:18:43 -0500

    day 9

--- 2022/09/aoc09.py
+++ 2022/09/aoc09.py
@@ -41,39 +41,15 @@ def yank(head, tail):
         tail[1] += 1 if head[1] > tail[1] else -1
 
 
-def print_grid(rope, height=5, width=6):
-    print()
-    for x in reversed(range(height)):
-        for y in range(width):
-            for knot, coords in enumerate(rope):
-                if coords == [x,y]:
-                    print(knot if knot else 'H', end='')
-                    break
-            else:
-                print('.', end='')
-        print()
-    print()
-
-
 def play(rope, input) -> int:
-    """Play the input instructions on the given rope, return number of spaces visited by tail knot"""
     visited = set()
-    print_grid(rope)
-    print()
 
     for direction, steps in input:
-        print('----------------')
-        print(f'rope: {rope}')
-        print(f'   -> {direction} {steps}')
-
         for i in range(steps):
             move(rope[0], direction)
             for head, tail in pairwise(rope):
                 yank(head, tail)
             visited.add(tuple(rope[-1]))
-
-        print_grid(rope)
-        print()
 
     return len(visited)
 
