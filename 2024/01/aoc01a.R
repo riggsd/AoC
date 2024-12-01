d8807820e09e2fe989d0b71ae26ba3379e1eb9f9
David A. Riggs <david.a.riggs@gmail.com>
2024-12-01 11:08:13 -0500

    refactor parts A and B into single R script

--- 2024/01/aoc01a.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Rscript
-
-# https://adventofcode.com/2024/day/1
-
-input <- read.table("input.txt", header = FALSE)
-a <- input[, 1]
-b <- input[, 2]
-print( sum(abs(sort(a) - sort(b))) )
