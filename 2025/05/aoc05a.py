9db181a766b8da1faa5a408a9496b9b7098e43c5
David A. Riggs <david.a.riggs@gmail.com>
2025-12-06 02:20:17 -0500

    advent of code day 5a

---
+++ 2025/05/aoc05a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# https://adventofcode.com/2025/day/5
+
+import sys
+
+
+def input():
+    """Return input as 2D binary int grid"""
+    infile = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin
+    ranges, ingredients = [], []
+    for line in infile:
+        if '-' in line:
+            ranges.append([int(v) for v in line.split('-')])
+        elif line.strip():
+            ingredients.append(int(line))
+    return ranges, ingredients
+
+
+ranges, ids = input()
+
+# Part 1
+fresh = 0
+for id in ids:
+    for start, end in ranges:
+        if start <= id <= end:
+            fresh += 1
+            break
+
+print("Fresh Ingredients:", fresh)
